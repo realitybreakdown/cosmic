d17cdfe69a96b4b47eed40eab9e32eaff7042813
Roslyn Saplicki <j.saplicki@gmail.com>
2018-11-09 09:22:13 -0800

    exposing api

---
+++ controllers/api/horoscopes.js
@@ -0,0 +1,19 @@
+var Horoscope = require('../../models/Horoscope');
+
+module.exports = {
+    getAllHoroscopes,
+    getOneHoroscope
+};
+
+function getAllHoroscopes(req, res, next) {
+    Horoscope.find({}, function(err, horoscopes) {
+        if (err) return next(err);
+        res.status(200).json(horoscopes);
+    });
+}
+function getOneHoroscope(req, res, next) {
+    Horoscope.findById(req.params.id, function(err, horoscope) {
+        if (err) return next(err);
+        res.status(200).json(horoscope);
+    });
+}
